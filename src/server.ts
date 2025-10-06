d5b6cf94318f38eb852b13e451f53f8347813315
anachica20 <anachica0220@gmail.com>
2025-10-06 16:14:52 -0500

    Para desplegar

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,10 @@ dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 PetHub Backend running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`🚀 PetHub Backend running on port ${PORT}`);
+  });
+}
+
+export default app;
